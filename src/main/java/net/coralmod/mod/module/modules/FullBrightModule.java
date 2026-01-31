827b748ec2437d8bd4c06cde2e2a50ed33de534f
jirmjahu <159639233+jirmjahu@users.noreply.github.com>
2026-01-31 13:09:14 +0100

    Fix issues with FullBright module

--- src/main/java/net/coralmod/mod/module/modules/FullBrightModule.java
+++ src/main/java/net/coralmod/mod/module/modules/FullBrightModule.java
@@ -2,23 +2,30 @@ package net.coralmod.mod.module.modules;
 
 import net.coralmod.mod.module.Module;
 import net.coralmod.mod.module.ModuleInfo;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.OptionInstance;
 
 @ModuleInfo(name = "FullBright", description = "Makes everything bright")
 public class FullBrightModule extends Module {
 
-    private double oldGamma;
+    private double oldGamma = -1;
 
-    @Override
-    public void onEnable() {
-        if (mc == null) {
-            return;
-        }
+    public FullBrightModule() {
+        ClientTickEvents.START_CLIENT_TICK.register(mc -> {
+            if (!isEnabled()) {
+                return;
+            }
 
-        final OptionInstance<Double> gamma = mc.options.gamma();
+            final OptionInstance<Double> gamma = mc.options.gamma();
 
-        oldGamma = gamma.get();
-        gamma.set(100000.0);
+            if (oldGamma == -1) {
+                oldGamma = gamma.get();
+            }
+
+            if (gamma.get() < 1000.0) {
+                gamma.set(100000.0);
+            }
+        });
     }
 
     @Override
@@ -27,6 +34,10 @@ public class FullBrightModule extends Module {
             return;
         }
 
-        mc.options.gamma().set(oldGamma);
+        if (oldGamma != -1) {
+            mc.options.gamma().set(oldGamma);
+        }
+
+        oldGamma = -1;
     }
 }
