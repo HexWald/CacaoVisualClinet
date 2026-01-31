79ae0c3793ea27628f4b1e64ac9736c0a3c2d917
jirmjahu <159639233+jirmjahu@users.noreply.github.com>
2026-01-31 16:32:57 +0100

    Remove chat signing indicators

--- src/main/java/net/coralmod/mod/mixin/ChatComponentMixin.java
+++ src/main/java/net/coralmod/mod/mixin/ChatComponentMixin.java
@@ -1,11 +1,16 @@
 package net.coralmod.mod.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.coralmod.mod.event.AddChatMessageEvent;
+import net.minecraft.client.GuiMessage;
+import net.minecraft.client.GuiMessageTag;
 import net.minecraft.client.gui.components.ChatComponent;
 import net.minecraft.network.chat.Component;
+import net.minecraft.util.FormattedCharSequence;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ChatComponent.class)
@@ -14,5 +19,10 @@ public class ChatComponentMixin {
     @Inject(method = "addMessage(Lnet/minecraft/network/chat/Component;)V", at = @At("HEAD"))
     public void onAddMessage(Component component, CallbackInfo info) {
         AddChatMessageEvent.ADD_CHAT_MESSAGE_EVENT.invoker().onChatMessage(component.getString());
+    }
+
+    @WrapOperation(method = "addMessageToDisplayQueue", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/GuiMessage;tag()Lnet/minecraft/client/GuiMessageTag;"))
+    private GuiMessageTag removeChatSigningIndicators(GuiMessage instance, Operation<GuiMessageTag> original) {
+        return null;
     }
 }
